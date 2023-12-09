0ffcdf28d4f5e419bcf79705731c662f1b9c22f7
ical <nukical@gmail.com>
2023-12-09 18:34:23 +0800

    Arduino control mode fixed

--- arduino/dc_control.ino
+++ arduino/dc_control.ino
@@ -1,9 +1,11 @@
-const char SHUTDOWN = '0';
-const char FORWARD = '1';
-const char BACKWARD = '2';
-const char SHIFT_LEFT = '3';
-const char SHIFT_RIGHT = '4';
-const char BRAKE = '5';
+const int SHUTDOWN = 0;
+const int FORWARD = 1;
+const int BACKWARD = 2;
+const int SHIFT_LEFT = 3;
+const int SHIFT_RIGHT = 4;
+const int BRAKE = 5;
+const int TURN_LEFT = 8;
+const int TURN_RIGHT = 9;
 
 // PIN
 const int SLP = 7;
@@ -16,15 +18,19 @@ const int C_DIR = 6;
 const int D_PWM = 2;  // Right rear wheel
 const int D_DIR = 3;
 
-char signal;
-char prevSig; // Previous signal
-char prevChangeSig; // Previous change signal
+int signal;
+char _;
 
 int speed = 0;
-int max_speed = 150;
-
-int acc = 30; // Acceleration
-int brake = 0;
+int max_speed = 200;
+int A_speed = 0;
+int B_speed = 0;
+int C_speed = 0;
+int D_speed =  0;
+int Amax_speed = 225;
+int Bmax_speed = 160;
+int Cmax_speed = 225;
+int Dmax_speed = 160;
 
 void setup() {
    Serial.begin(115200); // Set Baud rate
@@ -42,147 +48,88 @@ void setup() {
 void loop() {
   digitalWrite(SLP, HIGH); // Set HIGH to enable the motor driver
   
-  if (Serial.available() > 0) { // Receive signal
+  if (Serial.available()) { // Receive signal
     signal = Serial.read();
+    signal -=48;
+    _ = Serial.read();
+    Serial.println(signal);
   }
-
-  // Speed down if the signal is changed
-  if ((signal != BRAKE) && (signal != SHUTDOWN) && (prevSig != signal) && (prevSig != BRAKE) ){   
-    for (; speed > 0; speed -= acc) {
-      if (Serial.available() > 0) {   // Received signal while decelerating
-        signal = Serial.read();
-        if ((signal != BRAKE) && (signal != SHUTDOWN) && (prevChangeSig == signal)) { // If the signal and previous change signal are the same, stop decelerating
-          break;
-        }
-      }
-      ABCD_PWM(speed);
-      delay(100);
-    }
-    if (prevChangeSig != signal) {
-      speed = 0;
-      ABCD_PWM(speed);
-      delay(100);
-    }
-    prevSig = signal; 
-  }
-
-
 
   // Command handling
   switch (signal) {
     case SHUTDOWN:
       speed = 0;
-      ABCD_PWM(speed);
-      prevSig = signal;
+      ABCD_PWM(speed, speed, speed, speed);
       break;
 
     case FORWARD:     // Forward
       ABCD_DIR(HIGH, HIGH, HIGH, HIGH);
-      prevSig = signal;
-      prevChangeSig = signal;
-      for (; speed <= max_speed; speed += acc) {   // Slowly speed up
-        if (speed > max_speed) {
-          speed = max_speed;
-        }
-        brake = brake_judge();
-        if (brake == 1) {
-          break;
-        }
-        ABCD_PWM(speed);
-        delay(200);
-      }
+      A_speed = Amax_speed;
+      B_speed = Bmax_speed;
+      C_speed = Cmax_speed;
+      D_speed = Dmax_speed;
+      ABCD_PWM(A_speed, B_speed, C_speed, D_speed);
       break;
 
     case BACKWARD:      // Backward
+      A_speed = Amax_speed;
+      B_speed = Bmax_speed;
+      C_speed = Cmax_speed;
+      D_speed = Dmax_speed;
       ABCD_DIR(LOW, LOW, LOW, LOW);
-      prevSig = signal;
-      prevChangeSig = signal;
-      for (; speed <= max_speed; speed += acc) {
-        if (speed > max_speed) {
-          speed = max_speed;
-        }
-        brake = brake_judge();
-        if (brake == 1) {
-          break;
-        }
-        ABCD_PWM(speed);
-        delay(200);
-      }
+      speed = max_speed;
+      ABCD_PWM(A_speed, B_speed, C_speed, D_speed);
       break;
 
     case SHIFT_LEFT:      // Shift left
+      A_speed = 150;
+      B_speed = 170;
+      C_speed = 150;
+      D_speed = 150;
       ABCD_DIR(HIGH, LOW, LOW, HIGH);
-      prevSig = signal;
-      prevChangeSig = signal;
-      for (; speed <= max_speed; speed += acc) {
-        if (speed > max_speed) {
-          speed = max_speed;
-        }
-        brake = brake_judge();
-        if (brake == 1) {
-          break;
-        }
-        ABCD_PWM(speed);
-        delay(200);
-      }
+      speed = max_speed;
+      ABCD_PWM(A_speed, B_speed, C_speed, D_speed);
       break;
 
     case SHIFT_RIGHT:     // Shift right
       ABCD_DIR(LOW, HIGH, HIGH, LOW);
-      prevSig = signal;
-      prevChangeSig = signal;
-      for (; speed <= max_speed; speed += acc) {
-        if (speed > max_speed) {
-          speed = max_speed;
-        }
-        brake = brake_judge();
-        if (brake == 1) {
-          break;
-        }
-        ABCD_PWM(speed);
-        delay(200);
-      }
+      A_speed = 150;
+      B_speed = 170;
+      C_speed = 150;
+      D_speed = 150;
+      ABCD_PWM(A_speed, B_speed, C_speed, D_speed);
+      break;
+
+    case TURN_LEFT:     // Turn left
+      ABCD_DIR(LOW, HIGH, LOW, HIGH);
+      A_speed = 200;
+      B_speed = 200;
+      C_speed = 200;
+      D_speed = 200;
+      ABCD_PWM(A_speed, B_speed, C_speed, D_speed);
+      break;
+
+    case TURN_RIGHT:     // Turn right
+      ABCD_DIR(HIGH, LOW, HIGH, LOW);
+      A_speed = 200;
+      B_speed = 200;
+      C_speed = 200;
+      D_speed = 200;
+      ABCD_PWM(A_speed, B_speed, C_speed, D_speed);
       break;
 
     case BRAKE:     // Brake
-      for (; speed > 0; speed -= acc) {
-        if (Serial.available() > 0) {
-          signal = Serial.read();
-          if ((signal != BRAKE) && (signal != SHUTDOWN) && (prevChangeSig == signal)) {
-              break;
-          }
-        }
-        ABCD_PWM(speed);
-        delay(100);
-      }
-      if (prevChangeSig != signal) {
-        speed = 0;
-        ABCD_PWM(speed);
-        delay(100);
-      }
-      prevSig = signal;
+      speed = 0;
+      ABCD_PWM(speed, speed, speed, speed);
       break;
   }
 }
 
-int brake_judge() {
-  int brake = 0;
-  if (Serial.available() > 0) {
-    signal = Serial.read();
-    if ((prevSig != signal) || (prevChangeSig == signal)) {
-      brake = 1;
-    } else {
-      brake = 0;
-    }
-  }
-  return brake;
-}
-
-void ABCD_PWM(int speed) {
-  analogWrite(A_PWM, speed);
-  analogWrite(B_PWM, speed);
-  analogWrite(C_PWM, speed);
-  analogWrite(D_PWM, speed);
+void ABCD_PWM(int A_speed, int B_speed, int C_speed, int D_speed) {
+  analogWrite(A_PWM, A_speed);
+  analogWrite(B_PWM, B_speed);
+  analogWrite(C_PWM, C_speed);
+  analogWrite(D_PWM, D_speed);
 }
 
 void ABCD_DIR(int A_HL, int B_HL, int C_HL, int D_HL) {
--- arduino/servo_control.ino
+++ arduino/servo_control.ino
@@ -2,62 +2,58 @@
 
 Servo shootGate;  // 建立servo物件
 Servo shootRod;
-Servo supplyRod;
+//Servo supplyRod;
 
 int signal;
 char _;
+int gate = 0;
 
 const int SHUTDOWN = 0;
 const int FIRE = 6;
-const int RELOAD = 7;
+const int OPENGATE = 7;
 
 void setup() {
   Serial.begin(115200); // Set Baud rate
-  shootGate.attach(8);  // PIN for the launching gate
-  shootRod.attach(9);   // PIN for the launching rod
-  supplyRod.attach(10);  // PIN for the supplementary rod
+  shootGate.attach(10);  // PIN for the launching gate
+  shootRod.attach(11);   // PIN for the launching rod
+//  supplyRod.attach(8);  // PIN for the supplementary rod
   shootGate.write(35);
   delay(200);
   shootRod.write(180);
-  delay(200);
-  supplyRod.write(0);
+//  delay(200);
+//  supplyRod.write(0);
 }
 
 void loop() {
-  if(Serial.available()){
+  if (Serial.available()) {
     signal = Serial.read();
     signal -= 48;
     _ = Serial.read();
-    if(signal == FIRE){
-      shootGate.write(0);
-      delay(200);
+    if (signal == OPENGATE) {
+      delay(100);
+      if (gate == 0) {
+        shootGate.write(0);
+        delay(200);
+        gate = 1;
+
+      }
+      else if (gate == 1) {
+        shootGate.write(35);
+        delay(200);
+        gate = 0;
+      }
+    } else if (signal == FIRE) {
+      delay(100);
       shootRod.write(80);
       delay(800);
       shootRod.write(180);
-      delay(800);
+      delay(1300);
+      
+    } else if (signal == SHUTDOWN) {
       shootGate.write(35);
-      delay(200);
-    }else if(signal == RELOAD){
-      shootGate.write(0);
       delay(200);
       shootRod.write(80);
-      delay(800);
-      supplyRod.write(80);
-      delay(800);
-      supplyRod.write(0);
-      delay(1000);
-      shootRod.write(180);
-      delay(800);
-      shootGate.write(35);
-      delay(200);
-    }else if(signal == SHUTDOWN){
-      shootGate.write(35);
-      delay(200);
-      shootRod.write(180);
-      delay(800);
-      supplyRod.write(0);
       delay(800);
     }
   }
-
 }
