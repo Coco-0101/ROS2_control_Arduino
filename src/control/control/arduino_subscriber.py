d52c103a5cf4f1c5007ad3042ffd6abb745ec9ca
ical <nukical@gmail.com>
2023-12-27 15:11:36 +0800

    stupid bug fixed

--- src/control/control/arduino_subscriber.py
+++ src/control/control/arduino_subscriber.py
@@ -2,89 +2,55 @@ import rclpy
 import time
 from rclpy.node import Node
 from std_msgs.msg import Int8
-from std_msgs.msg import String ,Int8
+from std_msgs.msg import String, Int8
 import serial
-
-serial_comm_1 = serial.Serial()
-serial_comm_2 = serial.Serial()
 
 class MinimalSubscriber(Node):
     def __init__(self):
         super().__init__("minimal_subscriber")
-        self.subscription = self.create_subscription(Int8, "controller_signal", self.callback, 10)
+
         self.get_logger().info("Subscriber is listening...")
 
-        # Declare parameters for serial ports and baud rates
-        self.declare_parameter("serial_port1", "/dev/ttyACM0")
-        self.declare_parameter("baud_rate1", 115200)
-        self.declare_parameter("subscribe_to1", "arduino_command1")
-        self.declare_parameter("serial_port2", "/dev/ttyACM1")
-        self.declare_parameter("baud_rate2", 115200)
-        self.declare_parameter("subscribe_to2", "arduino_command2")
+        # Declare parameters for serial port and baud rate
+        sp1 = "/dev/ttyACM1"
+        sp2 = "/dev/ttyACM0"
+        br = 115200
 
-        sp1, br1, sub1, sp2, br2, sub2 = self.read_parameters()
+        # Create a serial_comm object for each instance
+        self.serial_comm1 = serial.Serial()
+        self.serial_comm2 = serial.Serial()
 
-        # Connect to given ports at given baud rates
-        self.connect_serial_port_1(serial_port=sp1, baud_rate=br1)
-        self.connect_serial_port_2(serial_port=sp2, baud_rate=br2)
+        self.subscription = self.create_subscription(Int8, "controller_signal", self.write_serial_data, 10)
+        self.connect_serial_port(serial_port=sp1, baud_rate=br, serial_comm=self.serial_comm1)
+        self.connect_serial_port(serial_port=sp2, baud_rate=br, serial_comm=self.serial_comm2)
 
-        time.sleep(0.1) # Wait for connection.
-
-        self.data_publisher_1 = self.create_publisher(String, "arduino_command1", 10) # Start publising serial port data.
-        self.data_publisher_2 = self.create_publisher(String, "arduino_command2", 10) # Start publising serial port data.
+        time.sleep(0.1)  # Wait for connection.
 
         self.get_logger().info("Serial communication has started.")
-        self.timer_ = self.create_timer(0.01, self.read_serial_data)
 
-    def read_parameters(self):
-        serial_port_1 = self.get_parameter("serial_port1").get_parameter_value().string_value
-        baud_rate_1 = self.get_parameter("baud_rate1").get_parameter_value().integer_value
-        subscribe_1 = self.get_parameter("subscribe_to1").get_parameter_value().string_value
+    def connect_serial_port(self, serial_port, baud_rate, serial_comm):
+        serial_comm.port = serial_port
+        serial_comm.baudrate = baud_rate
+        serial_comm.timeout = 1
+        if not serial_comm.is_open:
+            serial_comm.open()
 
-        serial_port_2 = self.get_parameter("serial_port2").get_parameter_value().string_value
-        baud_rate_2 = self.get_parameter("baud_rate2").get_parameter_value().integer_value
-        subscribe_2 = self.get_parameter("subscribe_to2").get_parameter_value().string_value
-
-        self.get_logger().info("Port: " + serial_port_1 + " Baud Rate: " + str(baud_rate_1) + " Subscribed To: " + subscribe_1)
-        self.get_logger().info("Port: " + serial_port_2 + " Baud Rate: " + str(baud_rate_2) + " Subscribed To: " + subscribe_2)
-        
-
-        return serial_port_1, baud_rate_1, subscribe_1, serial_port_2, baud_rate_2, subscribe_2
-
-    def connect_serial_port_1(self, serial_port, baud_rate):
-        serial_comm_1.port = serial_port
-        serial_comm_1.baudrate = baud_rate
-        serial_comm_1.timeout = 0.05
-        serial_comm_1.open()
-
-    def connect_serial_port_2(self, serial_port, baud_rate):
-        serial_comm_2.port = serial_port
-        serial_comm_2.baudrate = baud_rate
-        serial_comm_2.timeout = 0.05
-        serial_comm_2.open()
-
-    def read_serial_data(self):
+    def write_serial_data(self, msg: String):
         try:
-            msg = String()
-            msg.data = serial_comm_1.readline().decode("utf-8").rstrip("\n").rstrip("\r")
-            msg.data = serial_comm_2.readline().decode("utf-8").rstrip("\n").rstrip("\r")
-            self.data_publisher_1.publish(msg)
-            self.data_publisher_2.publish(msg)
+            serial_data = str(msg.data)
+            self.serial_comm1.write(serial_data.encode("utf-8"))
+            self.serial_comm2.write(serial_data.encode("utf-8"))
+            self.get_logger().info("Received signal: {0}".format(serial_data))
         except Exception as e:
-            self.get_logger().warn("Serial communication error. -Reading")
+            self.get_logger().warn("Serial communication error. -Writing")
             self.get_logger().warn(repr(e))
-
-    def callback(self, msg):
-        signal_value = str(msg.data)
-        serial_comm_1.write(signal_value.encode("utf-8"))
-        serial_comm_2.write(signal_value.encode("utf-8"))
-        self.get_logger().info("Received signal: {0}".format(msg.data))
 
 
 def main(args=None):
     rclpy.init(args=args)
     minimal_subscriber = MinimalSubscriber()
     rclpy.spin(minimal_subscriber)
+    minimal_subscriber.destroy_node()
     rclpy.shutdown()
 
 if __name__ == "__main__":
